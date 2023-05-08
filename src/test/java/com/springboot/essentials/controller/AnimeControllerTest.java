a0c00d7e8df313ddf666ccbea6bf48fbe48a8a2e
iagatha <iagathasouzaa@gmail.com>
2023-05-08 18:34:16 -0300

    controller

---
+++ src/test/java/com/springboot/essentials/controller/AnimeControllerTest.java
@@ -0,0 +1,25 @@
+package com.springboot.essentials.controller;
+
+import com.springboot.essentials.service.AnimeService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+class AnimeControllerTest {
+
+    @InjectMocks
+    private AnimeController animeController;
+
+    @Mock
+    private AnimeService animeService;
+
+    @Test
+    void  test(){
+
+    }
+}
